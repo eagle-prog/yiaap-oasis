0f05fd0f1573f84e619b13ae6a4c75d507ec17b5
Eagle19243 <eagle19243@gmail.com>
2021-04-02 03:10:57 -0400

    un-ignore files

---
+++ work_directory/Profile.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * SeekQuarry/Yioop --
+ * Open Source Pure PHP Search Engine, Crawler, and Indexer
+ *
+ * Copyright (C) 2009-2012  Chris Pollett chris@pollett.org
+ *
+ * LICENSE:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * END LICENSE
+ *
+ * @author Chris Pollett chris@pollett.org
+ * @license https://www.gnu.org/licenses/ GPL3
+ * @link https://www.seekquarry.com/
+ * @copyright 2009-2012
+ * @filesource
+ */
+namespace seekquarry\yioop\configs;
+
+/**
+ * Computer generated file giving the key defines of directory locations
+ * as well as database settings used to run the SeekQuarry/Yioop search engine
+ */
+nsdefine('AD_LOCATION', "none");
+nsdefine('API_ACCESS', "true");
+nsdefine('AUTH_KEY', "2x1J6WYaOq0");
+nsdefine('AUTOLOGOUT', "86400");
+nsdefine('BACKGROUND_COLOR', "#87ceea");
+nsdefine('BACKGROUND_IMAGE', "wd/resources/oasis.jpg");
+nsdefine('CACHE_LINK', "true");
+nsdefine('CAPTCHA_MODE', "2");
+nsdefine('CONFIGURE_BOT', "enable_bot_users");
+nsdefine('COOKIE_LIFETIME', "31536000");
+nsdefine('CSRF_TOKEN', "YIOOP_TOKEN");
+nsdefine('DEBUG_LEVEL', 6);
+nsdefine('DESCRIPTION_WEIGHT', "1");
+nsdefine('DB_HOST', "");
+nsdefine('DBMS', "Sqlite3");
+nsdefine('DB_NAME', "public_default");
+nsdefine('DB_PASSWORD', "");
+nsdefine('DB_USER', "");
+nsdefine('DEFAULT_LOCALE', "en-US");
+nsdefine('DIFFERENTIAL_PRIVACY', "1");
+nsdefine('FAVICON', "wd/resources/favicon1111.png");
+nsdefine('FOREGROUND_COLOR', "#ffffff");
+nsdefine('GLOBAL_ADSCRIPT', "");
+nsdefine('GROUP_ITEM', "");
+nsdefine('GROUP_ANALYTICS_MODE', "1");
+nsdefine('IN_LINK', "true");
+nsdefine('IP_LINK', "true");
+nsdefine('LANDING_PAGE', "");
+nsdefine('LINK_WEIGHT', "2");
+nsdefine('LOGO_SMALL', "wd/resources/logo-header-dark.png");
+nsdefine('LOGO_MEDIUM', "wd/resources/logo-header-dark.png");
+nsdefine('LOGO_LARGE', "wd/resources/logo-header-dark.png");
+nsdefine('MAIL_PASSWORD', "");
+nsdefine('MAIL_SECURITY', "");
+nsdefine('MAIL_SENDER', "admin@yiaap.com");
+nsdefine('MAIL_SERVER', "");
+nsdefine('MAIL_SERVERPORT', "");
+nsdefine('MAIL_USERNAME', "");
+nsdefine('MIN_RESULTS_TO_GROUP', "200");
+nsdefine('MONETIZATION_TYPE', "no_monetization");
+nsdefine('MEDIA_MODE', "name_server");
+nsdefine('NAME_SERVER', "http://localhost/");
+nsdefine('PRIVATE_DB_NAME', "private_default");
+nsdefine('PRIVATE_DB_HOST', "");
+nsdefine('PRIVATE_DBMS', "sqlite3");
+nsdefine('PRIVATE_DB_PASSWORD', "");
+nsdefine('PRIVATE_DB_USER', "");
+nsdefine('PROXY_SERVERS', "");
+nsdefine('RECOVERY_MODE', "1");
+nsdefine('REGISTRATION_TYPE', "no_activation");
+nsdefine('RESULT_SCORE', "true");
+nsdefine('ROBOT_INSTANCE', "www_yiaap_com-1614936208");
+nsdefine('RSS_ACCESS', "true");
+nsdefine('SEARCH_ANALYTICS_MODE', "1");
+nsdefine('SEARCHBAR_PATH', "");
+nsdefine('SEND_MAIL_MEDIA_UPDATER', "");
+nsdefine('SESSION_NAME', "yioopbiscuit");
+nsdefine('SIDE_ADSCRIPT', "");
+nsdefine('SIDEBAR_COLOR', "#f8f8f8");
+nsdefine('SIGNIN_LINK', "true");
+nsdefine('SIMILAR_LINK', "true");
+nsdefine('SUBSEARCH_LINK', "true");
+nsdefine('TIMEZONE', "America/Los_Angeles");
+nsdefine('TITLE_WEIGHT', "4");
+nsdefine('TOPBAR_COLOR', "#f5f5ff");
+nsdefine('TOP_ADSCRIPT', "");
+nsdefine('TOR_PROXY', "127.0.0.1:9150");
+nsdefine('USE_FILECACHE', "");
+nsdefine('USE_MAIL_PHP', "true");
+nsdefine('USE_PROXY', "");
+nsdefine('USER_AGENT_SHORT', "Please Name Your robot");
+nsdefine('WEB_URI', "/");
+nsdefine('WEB_ACCESS', "true");
+nsdefine('WORD_SUGGEST', "true");
---
+++ work_directory/_placeholder.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * SeekQuarry/Yioop --
+ * Open Source Pure PHP Search Engine, Crawler, and Indexer
+ *
+ * Copyright (C) 2009 - 2021  Chris Pollett chris@pollett.org
+ *
+ * LICENSE:
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * END LICENSE
+ *
+ * @author Chris Pollett chris@pollett.org
+ * @license https://www.gnu.org/licenses/ GPL3
+ * @link https://www.seekquarry.com/
+ * @copyright 2009-2017
+ * @filesource
+ */
+namespace seekquarry\yioop\configs;
+
+/**
+ * File used before work_directory auto-populated so that git knows about
+ * work_directory
+ */
